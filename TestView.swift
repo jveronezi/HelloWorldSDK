715ea0d5918d3c21ca09916727756f155865a76b
jveronezi <jveronezi@fullstacklabs.co>
2023-12-27 15:04:35 -0300

    adding a new class

---
+++ TestView.swift
@@ -0,0 +1,15 @@
+//
+//  TestView.swift
+//  HelloWorldSDK
+//
+//  Created by Joao victor Veronezi on 27/12/23.
+//
+
+import SwiftUI
+
+public struct TestView: View {
+    public init() {}
+    public var body: some View {
+        Text("Test")
+    }
+}
