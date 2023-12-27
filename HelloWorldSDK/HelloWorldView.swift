08d56adf8d1dc7fb3bbd84119241a2ccb0461bb9
jveronezi <jveronezi@fullstacklabs.co>
2023-12-27 14:56:27 -0300

    adding a new class

---
+++ HelloWorldSDK/HelloWorldView.swift
@@ -0,0 +1,29 @@
+//
+//  HelloWorldView.swift
+//  HelloWorldSDK
+//
+//  Created by Joao victor Veronezi on 27/12/23.
+//
+
+import SwiftUI
+
+public struct HelloWorldView: View {
+    public init() {}
+    
+    @State private var isPresentWebView = false
+
+    public var body: some View {
+        Button("Open WebView") {
+            isPresentWebView = true
+
+        }
+        .sheet(isPresented: $isPresentWebView) {
+            NavigationView {
+                WebView(url: URL(string: "https://google.com")!)
+                    .ignoresSafeArea()
+                    .navigationTitle("Click here")
+                    .navigationBarTitleDisplayMode(.inline)
+            }
+        }
+    }
+}
