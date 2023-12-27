da87f1d02f5aff9446f7e51f948fd6b738d3867c
jveronezi <jveronezi@fullstacklabs.co>
2023-12-27 11:21:36 -0300

    initial commit

---
+++ HelloWorldSDK/HelloWorld.swift
@@ -0,0 +1,16 @@
+//
+//  HelloWorld.swift
+//  HelloWorldSDK
+//
+//  Created by Joao victor Veronezi on 27/12/23.
+//
+
+import Foundation
+
+public class MainHelloWorld {
+    public init() {}
+    
+    public func helloWorld() -> String {
+        return "Hello World"
+    }
+}
